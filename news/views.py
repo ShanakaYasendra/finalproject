035ee86c260c5b0652ed128a2b3663d2102a1012
Shanaka Kalubowilage <shan@Shanakas-Air.gateway>
2020-07-24 10:05:58 +1000

    add travel advice

--- news/views.py
+++ news/views.py
@@ -4,7 +4,7 @@ import requests
 
 # Create your views here.
 def index(request):
-    country='AU'
+    country='us'
     url='https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=28719ed44bffb67e022e502067349ca0'
     city= 'Melbourne, AU'
     r= requests.get(url.format(city)).json()
@@ -16,21 +16,23 @@ def index(request):
     con_pop_url='http://api.worldbank.org/v2/country/{}/indicator/SP.POP.TOTL?per_page=1&format=json'
    
     con_pop_res= requests.get(con_pop_url.format(country)).json()
-    print(con_pop_res)
+    #print(con_pop_res)
     news_url='https://newsapi.org/v2/top-headlines?country={}&apiKey=391409f008894221b310aec8d3d276d5'
     #'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=391409f008894221b310aec8d3d276d5'
     #https://newsapi.org/v2/top-headlines?country=us&apiKey=391409f008894221b310aec8d3d276d5
     news_response=requests.get(news_url.format(country)).json()
     #print(news_response)
 
-
+    travel_url='https://www.travel-advisory.info/api?countrycode={}?format=json'
+    travel_res=requests.get(travel_url.format(country)).json()
+    print(travel_res)
 
     country_de={
         'region': cr[1],
         #'latitude':cr[1]['latitude']
     }
 
-    
+   
     
     city_weather={
         'city':city,
@@ -40,6 +42,9 @@ def index(request):
 
 
     }
+    country_travel_advice={
+
+    }
     country_news={
         'total': news_response['totalResults'],
         'articles':news_response['articles']
@@ -47,8 +52,9 @@ def index(request):
     context={
         'city_weather': city_weather,
         'country_news':country_news,
-
-        'country': country_de
+        'country_population': con_pop_res,
+        'country': country_de,
+        'travel_advice':travel_res
 
     }
    
